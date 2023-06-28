9d631e9b57d3d9024fbbc8d827ab8a28a108c3c1
DaEvIg <danilevdokimov96@example.com>
2023-06-28 18:53:44 +0300

    test model.read-update-delete

--- model.py
+++ model.py
@@ -8,19 +8,59 @@ class Model(Connection):
             con = self.connect()
             cur = self.con.cursor()
             cur.execute("CREATE TABLE IF NOT EXISTS User(login, password, name, e_mail)")
-            cur.execute("INSERT INTO User(login, password, name, e_mail) VALUES(?,?,?,?)", *args)
             con.commit()
             print("таблица успешно создана")
         except sqlite3.Error as err:
             print(f"не удалось создать таблицу {err}")
+        finally:
+            self.con.close()
     def read(self):
-        pass
-    def update(self):
-        pass
-    def delete(self):
-        pass
+        try:
+            con = self.connect()
+            cur = self.con.cursor()
+            cur.execute("SELECT name FROM User")
+            return cur.fetchall()
+        except sqlite3.Error as err:
+            print(f"не удалось сделать выборку {err}")
+        finally:
+            self.con.close()
+    def update(self, *args):
+        try:
+            con = self.connect()
+            cur = self.con.cursor()
+            cur.execute("INSERT INTO User(login, password, name, e_mail) VALUES(?,?,?,?)", *args)
+            con.commit()
+        except sqlite3.Error as err:
+            print(f"не удалось внести изменение в таблицу {err}")
+        finally:
+            self.con.close()
+    def delete(self, *args):
+        try:
+            con = self.connect()
+            cur = self.con.cursor()
+            cur.execute("DELETE FROM User WHERE name = (?)", *args)
+            con.commit()
+        except sqlite3.Error as err:
+            print(f"не удалось удалить данные таблицы{err}")
+        finally:
+            self.con.close()
 
-# Model test
+
+# Model test create
 # x = Model()
 # args = ["daevig", "12345", "Danil", "d@mail.ru"]
 # x.create(args)
+
+# Model test read
+#y = Model()
+#y.read()
+
+# Model test update
+#z = Model()
+#args = ["qwerty", "54321", "Sergey", "s@gmail.com"]
+#z.update(args)
+
+# Model test delete
+# p = Model()
+# args = ["Danil"]
+# p.delete(args)
